34a1445fcb6d87dabd07451d72034b278aebd94d
Ngoc Anh <ngocanh@MacBook-Air-cua-Ngoc.local>
2018-10-18 13:01:27 +0700

    Please help review

--- Lab3/Homework/serious11.py
+++ Lab3/Homework/serious11.py
@@ -1,1 +1,14 @@
-def is_inside(a,h)
+x = int(input("Please input x axis: "))
+y = int(input("Please input y axis: "))
+
+a = [x,y]
+h = [140, 60, 100, 200]
+
+def is_inside(a,h):
+    if 140 < x < 240 and 60 < y < 260:
+        print("TRUE")
+    else:
+        print("FALSE")
+    return a
+    
+print(is_inside(a,h))
